90e267dd07a7ee6b3efc1186084e0f899c31fb80
Viktor Holovin <holovin@mail.com>
2022-02-18 09:32:14 +0100

    Delete services fetch

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,11 @@ import NavBar from '../components/NavBar.js'
 
 export async function getStaticProps() {
   const resSite = await fetch(process.env.API_URL)
-  const resServices = await fetch(process.env.API_URL + "services")
   const site = await resSite.json()
-  const services = await resServices.json()
 
   return {
     props: {
       site,
-      services,
     },
   }
 }
